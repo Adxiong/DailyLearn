6ede022df11235bbf5dc2819a56353996b30f7d9
adxiong <260245435@qq.com>
2022-01-08 00:53:29 +0800

    column 实现瀑布流

--- FE/瀑布流布局/columb.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Descripttion: 
- * @version: 
- * @Author: Adxiong
- * @Date: 2022-01-07 23:37:02
- * @LastEditors: Adxiong
- * @LastEditTime: 2022-01-07 23:37:02
- */
--- FE/瀑布流布局/http.js
+++ FE/瀑布流布局/http.js
@@ -4,9 +4,9 @@
  * @Author: Adxiong
  * @Date: 2022-01-07 23:37:10
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-01-08 00:24:50
+ * @LastEditTime: 2022-01-08 00:30:20
  */
-const axios = requir('axios')
+const axios = require('axios')
 const url = 'http://api.isoyu.com/api/picture/index'
 
 async function getPictures(page=20) {
